322572ed2e53cd2d54ba88f158d90db5efcfec1f
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2019-03-13 13:12:42 +0100

    errorMargin

--- src/main/java/logic/ILogic.java
+++ src/main/java/logic/ILogic.java
@@ -5,12 +5,11 @@ import dal.IWeightDAO;
 import java.io.IOException;
 
 public interface ILogic {
-    static double errorMargin = 0;
+    final static double errorMargin = 0.01;
     void work() throws IOException;
     void setDAO(IWeightDAO dao);
     String userAuthorization(int id); //returns username to display on scale, 11-99
     String enterBatchNumber(int batchNumber); // returns name corresponding to batchNumber, 1000-9999
-    void enterWeight(double weight);
     int getWeight();
     boolean validateResult(double netto, double brutto);
 }
