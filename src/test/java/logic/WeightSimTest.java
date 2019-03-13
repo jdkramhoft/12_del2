9d3bb11875842cb99da0e895ee197c75bb1e9d0a
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2019-03-13 13:28:54 +0100

    Initial test setup

---
+++ src/test/java/logic/WeightSimTest.java
@@ -0,0 +1,52 @@
+package logic;
+
+import dal.DatabaseImplementation;
+import dal.IWeightDAO;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeightSimTest {
+    /*
+    private ILogic sim;
+    private IWeightDAO dao;
+    private int invalidID = 1;
+
+
+    //Hardcoded operator
+    private int validID = 12;
+    private String opName = "Anders And";
+
+    //Hardcoded batch
+    private
+
+    @Before
+    public void setUp(){
+        this.dao = new DatabaseImplementation();
+        this.sim = new WeightSim();
+        sim.setDAO(dao);
+    }
+
+    @After
+    public void tearDown(){
+        this.dao = null;
+        this.sim = null;
+    }
+
+    @Test
+    public void shouldAuthorizeUser(){
+        String name = sim.userAuthorization(validID);
+        assertEquals(opName, name);
+    }
+
+    @Test
+    public void shouldNotAuthorizeUser(){
+        String name = sim.userAuthorization(invalidID);
+        assertNull(name);
+    }
+
+    */
+
+}
