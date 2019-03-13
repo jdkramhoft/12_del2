983a822cae9022febadb1c2ab3e5ce3edcfad722
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2019-03-13 14:14:33 +0100

    Updated WeightSimTest methods

--- src/test/java/logic/WeightSimTest.java
+++ src/test/java/logic/WeightSimTest.java
@@ -2,6 +2,8 @@ package logic;
 
 import dal.DatabaseImplementation;
 import dal.IWeightDAO;
+import dto.BatchDTO;
+import dto.UserDTO;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,44 +11,48 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WeightSimTest {
-    /*
+
     private ILogic sim;
     private IWeightDAO dao;
     private int invalidID = 1;
-
-
-    //Hardcoded operator
-    private int validID = 12;
-    private String opName = "Anders And";
-
-    //Hardcoded batch
-    private
+    private BatchDTO batch;
+    private UserDTO operator;
 
     @Before
     public void setUp(){
         this.dao = new DatabaseImplementation();
         this.sim = new WeightSim();
         sim.setDAO(dao);
+
+        //Hardcoded operator
+        operator = new UserDTO(12, "Anders And");
+
+        //Hardcoded batchID and name
+        this.batch = new BatchDTO();
+        batch.setId(1234);
+        batch.setName("Salt");
     }
 
     @After
     public void tearDown(){
         this.dao = null;
         this.sim = null;
+        this.batch = null;
+        this.operator = null;
+    }
+/*
+    @Test
+    public void shouldGetUser(){
+        UserDTO user = sim.userAuthorization(operator.getId());
+        assertEquals(operator, user);
     }
 
     @Test
-    public void shouldAuthorizeUser(){
-        String name = sim.userAuthorization(validID);
-        assertEquals(opName, name);
+    public void shouldReturnNull(){
+        UserDTO user = sim.userAuthorization(invalidID);
+        assertNull(user);
     }
+*/
 
-    @Test
-    public void shouldNotAuthorizeUser(){
-        String name = sim.userAuthorization(invalidID);
-        assertNull(name);
-    }
-
-    */
 
 }
