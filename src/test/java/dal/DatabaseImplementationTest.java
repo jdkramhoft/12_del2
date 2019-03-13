cf9ac70190d9d1adee5e8935b63f0f9266567694
Razmuz99 <razmuz99@gmail.com>
2019-03-13 15:11:46 +0100

    Test class setup

--- src/test/java/dal/DatabaseImplementationTest.java
+++ src/test/java/dal/DatabaseImplementationTest.java
@@ -1,8 +1,47 @@
 package dal;
 
+import dto.BatchDTO;
+import dto.UserDTO;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DatabaseImplementationTest {
 
+    DatabaseImplementation Database = new DatabaseImplementation();
+    UserDTO testUser = new UserDTO(1, "Bente Bent");
+    BatchDTO testBatch = new BatchDTO(1, "Sukker", 1, 23.32);
 
+    @Test
+    public void shouldGetUser() {
+
+    }
+
+    @Test
+    public void shouldGetBatch() {
+    }
+
+    @Test
+    public void ShouldCreateUser() {
+    }
+
+    @Test
+    public void shouldDeleteUser() {
+    }
+
+    @Test
+    public void shouldCreateBatch() {
+    }
+
+    @Test
+    public void shouldDeleteBatch() {
+    }
+
+    @Test
+    public void shouldUpdateUser() {
+    }
+
+    @Test
+    public void shouldUpdateBatch() {
+    }
 }
