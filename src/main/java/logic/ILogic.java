4456b453e7b8b4614e1e02882e209255daeeb173
Michael <mjarberg88@gmail.com>
2019-03-13 14:13:03 +0100

    moved TUI funktionallity to TUI

--- src/main/java/logic/ILogic.java
+++ src/main/java/logic/ILogic.java
@@ -3,10 +3,11 @@ package logic;
 import dal.IWeightDAO;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public interface ILogic {
     final static double ERROR_MARGIN = 0.0001;
-    void work() throws IOException;
+    Socket EstablishConnection() throws IOException;
     void setDAO(IWeightDAO dao);
     String userAuthorization(int id); //returns username to display on scale, 11-99
     String enterBatchNumber(int batchNumber); // returns name corresponding to batchNumber, 1000-9999
--- src/main/java/logic/WeightSim.java
+++ src/main/java/logic/WeightSim.java
@@ -3,31 +3,17 @@ package logic;
 import dal.DatabaseImplementation;
 import dal.IWeightDAO;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class WeightSim  implements ILogic{
+public class WeightSim implements ILogic{
     DatabaseImplementation DBConnection = new DatabaseImplementation();
 
     @Override
-    public void work() throws IOException {
-        String sentence;
-        String modifiedSentence;
+    public Socket EstablishConnection() throws IOException {
         Socket clientSocket;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader((System.in)));
         clientSocket = new Socket("127.0.0.1", 8000);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader((clientSocket.getInputStream())));
-        do{
-            sentence = inFromUser.readLine();
-            outToServer.writeBytes(sentence + '\n');
-            modifiedSentence = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + modifiedSentence);
-        } while(!sentence.equals("Q crlf"));
-        clientSocket.close();
+        return clientSocket;
     }
 
     @Override
